71137972f961645e981082c6aab6ffb281a42a69
LYJ <394492327@qq.com>
2026-05-01 21:43:40 +0800

    feat(html): add popovers for drumkit parts; fix: fix several known popover glitches.

--- src/animation-trigger.js
+++ src/animation-trigger.js
@@ -1,48 +1,53 @@
-export { triggerRandomAnimation };
+export { triggerRandomAnimation, triggerAnimation };
 
 const animations = ["heartbeat"];
 const sendedRequests = [];
 
 /**
- * 
- * @param {HTMLElement} element 
+ *
+ * @param {HTMLElement} element
  */
 function triggerRandomAnimation(element) {
-    if (sendedRequests.length >= 1) {
-        // This could happen when user trigger two animations at one browser tick,
-        // for example, by pressing two keys at once, 
-        // which will cause a glitch.
-        return;
-    }
+  triggerAnimation(element, animations[randInt(0, animations.length - 1)]);
+}
 
-    const randomAnimation = animations[randInt(0, (animations.length - 1))];
+function triggerAnimation(element, animationName) {
+  if (sendedRequests.length >= 1) {
+    // This could happen when user trigger two animations at one browser tick,
+    // for example, by pressing two keys at once,
+    // which will cause a glitch.
+    return;
+  }
 
-    const classes = element.classList;
-    for (const a of animations) {
-        classes.remove(a);
-    }
+  const classes = element.classList;
+  for (const a of animations) {
+    classes.remove(a);
+  }
 
-    sendedRequests.push(
-        requestAnimationFrame(() => {
-            classes.toggle(randomAnimation);
-            sendedRequests.pop();
-        })
-    );
-
+  sendedRequests.push(
+    requestAnimationFrame(() => {
+      classes.toggle(animationName);
+      sendedRequests.pop();
+    }),
+  );
 }
 
 function randInt(minIntIncluded, maxIntIncluded) {
-    if (!Number.isInteger(minIntIncluded) || !Number.isInteger(maxIntIncluded)) {
-        throw new TypeError(`Invalid argument error: both arguments has to be integer.`);
-    }
+  if (!Number.isInteger(minIntIncluded) || !Number.isInteger(maxIntIncluded)) {
+    throw new TypeError(
+      `Invalid argument error: both arguments has to be integer.`,
+    );
+  }
 
-    if (minIntIncluded > maxIntIncluded) {
-        throw new TypeError(`Invalid argument error: minIntIncluded has to be less or equal to maxIntIncluded`);
-    } else if (minIntIncluded === maxIntIncluded) {
-        return minIntIncluded;
-    }
+  if (minIntIncluded > maxIntIncluded) {
+    throw new TypeError(
+      `Invalid argument error: minIntIncluded has to be less or equal to maxIntIncluded`,
+    );
+  } else if (minIntIncluded === maxIntIncluded) {
+    return minIntIncluded;
+  }
 
-    let mathR = Math.random();
-    let decimalR = minIntIncluded + mathR * (maxIntIncluded - minIntIncluded);
-    return Math.round(decimalR);
+  let mathR = Math.random();
+  let decimalR = minIntIncluded + mathR * (maxIntIncluded - minIntIncluded);
+  return Math.round(decimalR);
 }
--- src/popover.js
+++ src/popover.js
@@ -1,43 +1,123 @@
 class PopoverManager {
-    #currentPoppedStack = new Array();
-    #registeredPopovers = new Array();
+  #currentPoppedStack = new Array();
+  #registeredPopovers = new Map(); // trigger : popover
 
-    constructor() {
-        // default close behavior
-        window.addEventListener("click", (triggerEvent) => this.#closeLastPopover(triggerEvent));
+  #isFreezing = false;
+
+  constructor() {
+    // default close behavior
+    window.addEventListener("click", (triggerEvent) =>
+      this.#closeLastPopover(triggerEvent),
+    );
+  }
+
+  #closeLastPopover = (triggerEvent) => {
+    if (this.#currentPoppedStack.length === 0) {
+      return;
     }
 
-    #closeLastPopover = (triggerEvent) => {
-        if (this.#currentPoppedStack.length === 0) {
-            return;
-        }
+    // only close if triggered outside the current popover area to avoid conflict
+    if (
+      this.#isClickedInsidePopover(
+        triggerEvent,
+        this.#currentPoppedStack.at(-1),
+      )
+    ) {
+      return;
+    }
 
-        // only close if triggered outside the current popover area to avoid conflict
-        if (this.#currentPoppedStack.at(-1) === triggerEvent.target) {
-            return;
-        }
+    const popover = this.#currentPoppedStack.pop();
+    popover.style = "display:none";
+    this.#isFreezing = false; // reenable other triggers.
+  };
 
-        const popover = this.#currentPoppedStack.pop();
-        popover.style = "display:none";
-    };
+  #isClickedInsidePopover = (clickEvent, popoverElement) => {
+    const rect = popoverElement.getBoundingClientRect();
+    const clickX = clickEvent.x;
+    const clickY = clickEvent.y;
 
-    addPopoverOnClick = (clickedElement, popoverDiv) => {
-        clickedElement.addEventListener("click", (ev) => {
-            popoverDiv.style = "";
-            this.#currentPoppedStack.push(popoverDiv);
-            ev.stopPropagation();
-        });
-        this.#registeredPopovers.push(popoverDiv);
-    };
+    if (
+      clickX > rect.left &&
+      clickX < rect.left + rect.width &&
+      clickY > rect.top &&
+      clickY < rect.top + rect.height
+    ) {
+      return true;
+    }
+    return false;
+  };
+
+  registerPopoverOnClick = (triggerElement, popoverDiv) => {
+    // main logic
+    triggerElement.addEventListener("click", (ev) => {
+      ev.stopPropagation();
+
+      if (this.#isFreezing) {
+        return;
+      }
+
+      if (this.#currentPoppedStack.includes(popoverDiv)) {
+        return;
+      }
+
+      popoverDiv.style = "";
+      this.#currentPoppedStack.push(popoverDiv);
+      this.#isFreezing = true; // to freeze other triggers.
+    });
+
+    // style logic
+    triggerElement.addEventListener("mouseover", () => {
+      if (this.#isFreezing) return;
+      triggerElement.classList.add("drumkit-popover-trigger__hover");
+    });
+    triggerElement.addEventListener("mouseleave", () => {
+      triggerElement.classList.remove("drumkit-popover-trigger__hover");
+    });
+
+    this.#registeredPopovers.set(triggerElement, popoverDiv);
+  };
 }
 
 const manager = new PopoverManager();
 
-manager.addPopoverOnClick(
-    document.querySelector("#save-preset-btn"),
-    document.querySelector("#save-preset-popover")
+manager.registerPopoverOnClick(
+  document.querySelector("#save-preset-btn"),
+  document.querySelector("#save-preset-popover"),
 );
-manager.addPopoverOnClick(
-    document.querySelector("#load-preset-btn"),
-    document.querySelector("#load-preset-popover")
+manager.registerPopoverOnClick(
+  document.querySelector("#load-preset-btn"),
+  document.querySelector("#load-preset-popover"),
+);
+
+manager.registerPopoverOnClick(
+  document.querySelector("#drumkit-popover-trigger-snare"),
+  document.querySelector("#drumkit-snare-popover"),
+);
+manager.registerPopoverOnClick(
+  document.querySelector("#drumkit-popover-trigger-hihat"),
+  document.querySelector("#drumkit-hihat-popover"),
+);
+manager.registerPopoverOnClick(
+  document.querySelector("#drumkit-popover-trigger-crash"),
+  document.querySelector("#drumkit-crash-popover"),
+);
+manager.registerPopoverOnClick(
+  document.querySelector("#drumkit-popover-trigger-tom1"),
+  document.querySelector("#drumkit-tom1-popover"),
+);
+manager.registerPopoverOnClick(
+  document.querySelector("#drumkit-popover-trigger-tom2"),
+  document.querySelector("#drumkit-tom2-popover"),
+);
+manager.registerPopoverOnClick(
+  document.querySelector("#drumkit-popover-trigger-tom3"),
+  document.querySelector("#drumkit-tom3-popover"),
+);
+manager.registerPopoverOnClick(
+  document.querySelector("#drumkit-popover-trigger-kick"),
+  document.querySelector("#drumkit-kick-popover"),
+);
+manager.registerPopoverOnClick(
+  document.querySelector("#drumkit-popover-trigger-ride"),
+  document.querySelector("#drumkit-ride-popover"),
 );
